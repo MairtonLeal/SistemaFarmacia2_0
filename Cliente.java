974f56ec316c0c38f670feb3972100337e8ad7d1
MairtonLeal <mairton.rodrigues12@hotmail.com>
2016-05-27 17:24:13 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mairton RL
+ */
+public class Cliente {
+    private String Nome;
+    private String cpf;
+    private String contato;
+    private String profissao;
+    private String saldo;
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the user
+     */
+   
+    /**
+     * @return the contato
+     */
+    public String getContato() {
+        return contato;
+    }
+
+    /**
+     * @param contato the contato to set
+     */
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    /**
+     * @return the profissao
+     */
+    public String getProfissao() {
+        return profissao;
+    }
+
+    /**
+     * @param profissao the profissao to set
+     */
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public String getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ DBConnection.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class DBConnection {
+public Connection connection = null;
+    private final String DRIVER = "com.mysql.jdbc.Driver";
+    private final String BDNAME = "sistemafarm";
+    private final String URL = "jdbc:mysql://localhost/" + BDNAME;  //jdbc:mysql://localhost/
+    private final String LOGIN = "root";
+    private final String SENHA = "mysql";
+    public boolean getConnection(){
+        
+        
+        
+        try{
+            Class.forName(DRIVER); //carrega o driver
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA); //estabelece a conexão por meio do obj connection
+            System.out.println("Conectou");
+            return true;
+        }catch(ClassNotFoundException erro){
+            System.out.println("Driver não encontrado " +erro.toString());
+            return false;
+        }catch(SQLException erro){
+            System.out.println("Falha ao conectar " +erro.toString());
+            return false;
+        }
+    }
+    public void close(){
+        try{
+            connection.close();
+            System.out.println("Desconectou");
+        }catch(SQLException erro){   
+        }
+    }
+   
+   
+    
+}
